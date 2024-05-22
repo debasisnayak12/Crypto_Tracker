d4fa2cda8ebbe0b67898ce2020d0ff3d4b517c02
debasisnayak <debasishnayak0739@gmail.com>
2024-05-22 12:27:26 +0530

    cmplt landing page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Common/Footer"
+import Header from "./components/Common/Header"
+import MainComponent from "./components/LandingPage/MainComponent"
+
+function App() {
+
+  return (
+   <div className="app">
+      <Header />
+      <MainComponent />
+      {/* <Footer /> */}
+   </div>
+  )
+}
+
+export default App

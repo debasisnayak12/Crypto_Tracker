1a0f11b84f654a6cd3abba55995df79fd8332cbf
debasisnayak <debasishnayak0739@gmail.com>
2024-05-30 09:42:03 +0530

    all done

--- src/components/Common/ShareButton/index.jsx
+++ src/components/Common/ShareButton/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton, FacebookIcon, TwitterIcon, WhatsappIcon } from "react-share";
 import Button from "../../Common/Button";
+import "./styles.css";
 
 const ShareButton = () => {
   const [showOptions, setShowOptions] = useState(false);
@@ -15,13 +16,13 @@ const ShareButton = () => {
       {showOptions && (
         <div className="share-options">
           <FacebookShareButton url={window.location.href}>
-            <FacebookIcon size={32} round />
+            <FacebookIcon size={40} round />
           </FacebookShareButton>
           <TwitterShareButton url={window.location.href}>
-            <TwitterIcon size={32} round />
+            <TwitterIcon size={40} round />
           </TwitterShareButton>
           <WhatsappShareButton url={window.location.href}>
-            <WhatsappIcon size={32} round />
+            <WhatsappIcon size={40} round />
           </WhatsappShareButton>
         </div>
       )}
